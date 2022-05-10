0a476a408cac07fe5f4d3e26b9685f4cc5289070
Daniil <ksusonic@yandex.ru>
2022-05-10 20:11:51 +0300

    new queue architecture

--- queue.go
+++ queue.go
@@ -1,17 +1,22 @@
 package main
 
+// In-memory queue //
+
 import (
 	"errors"
 	"gopkg.in/telebot.v3"
+	"strconv"
+	"strings"
 )
 
 type QueueMember struct {
-	Username  string   `json:"username"`
 	Usernames []string `json:"usernames"`
 	Message   string   `json:"message"`
 }
 
 type Queue struct {
+	Id      uint32         `json:"queue_id"`
+	Title   string         `json:"title"`
 	Chat    telebot.ChatID `json:"chat_id"`
 	Members []QueueMember  `json:"members"`
 }
@@ -20,20 +25,55 @@ func (q *Queue) Len() int {
 	return len(q.Members)
 }
 
-func (q *Queue) Swap(i, j int) {
+func (q *Queue) DebugString() string {
+	var sb strings.Builder
+
+	if len(q.Title) > 0 {
+		sb.WriteString("Title: " + q.Title + "\n\n")
+	} else {
+		sb.WriteString("Untitled queue\n")
+	}
+
+	for qi, m := range q.Members {
+		sb.WriteString("Members N" + strconv.Itoa(qi+1) + "\n")
+		for i, user := range m.Usernames {
+			sb.WriteString("  " + strconv.Itoa(i+1) + " @" + user + "\n")
+		}
+		if len(m.Message) != 0 {
+			sb.WriteString("with message: " + m.Message + "\n\n")
+		}
+	}
+
+	return sb.String()
+}
+
+func (q *Queue) Swap(ipos, jpos int) (bool, error) {
+	i, j := ipos-1, jpos-1
+	if i < 0 || j < 0 {
+		return false, errors.New("one of indexes <= 0")
+	}
+	if q.Len() <= i || q.Len() <= j {
+		return false, errors.New("one of indexes is out of range")
+	}
+
 	q.Members[i], q.Members[j] = q.Members[j], q.Members[i]
+	return true, nil
 }
 
 func (q *Queue) Push(x QueueMember) {
 	q.Members = append(q.Members, x)
 }
-func (q *Queue) Pop() (QueueMember, error) {
-	if len(q.Members) <= 1 {
-		return QueueMember{}, errors.New("empty queue")
+
+func (q *Queue) GetQueuePos(usernames []string) int {
+	// Returns position of user(s) in queue if exists. Else -1
+	for i, member := range q.Members {
+		for _, ruser := range member.Usernames {
+			for _, luser := range usernames {
+				if luser == ruser {
+					return i + 1
+				}
+			}
+		}
 	}
-	oldMembers := q.Members
-	n := len(oldMembers)
-	x := oldMembers[n-1]
-	q.Members = oldMembers[0 : n-1]
-	return x, nil
+	return -1
 }
